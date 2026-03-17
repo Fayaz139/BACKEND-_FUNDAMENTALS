06e968a264b6252285e417afe3db2cfabf701e0c
Fayaz139 <123862731+Fayaz139@users.noreply.github.com>
2026-03-17 16:46:25 +0530

    try to deploy

---
+++ todo-app/index.js
@@ -0,0 +1,15 @@
+// const express = require('express')
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/fayaz', (req, res) => {
+  res.json({query : 'Hello World im Fayaz'})
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
