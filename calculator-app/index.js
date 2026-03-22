32ecb8c04c29dd16255d5c763cab8ca170392109
Fayaz139 <123862731+Fayaz139@users.noreply.github.com>
2026-03-22 09:22:08 +0530

    tried something new

--- calculator-app/index.js
+++ calculator-app/index.js
@@ -4,7 +4,24 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.sendFile("[YOUR_PATH_FOR_INDEX.HTML]")
+  res.sendFile("/Users/sfayaz/100x/BACKEND-_FUNDAMENTALS/calculator-app/index.html")
+})
+
+app.use((req, res, next) => {
+  const a = req.body.n1;
+  const b = req.body.n2;
+
+  if (
+    n1 === undefined || n2 === undefined ||
+    n1 === "" || n2 === "" ||
+    isNaN(n1) || isNaN(n2)
+  ) {
+    next();
+  } else {
+      return res.status(400).json({
+        error: "Enter the number to Calculate"
+      })
+  }
 })
 
 app.post('/sum', (req, res) => {
@@ -32,7 +49,7 @@ app.post('/div', (req, res) => {
   const a = parseInt(req.body.n1);
   const b = parseInt(req.body.n2);
 
-  res.json({ans: a/b})
+  res.json({ans:  a/b})
 })
 
 app.listen(3000, () => {
